6a0523202fe39bd2e1131e4a0e694cddc38c6c90
Humberto Asca <hasca@ciplima.org.pe>
2023-11-12 19:56:33 -0500

    Layout of Login page.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,36 @@
+// max-w-sm
 const page = () => {
     return (
         <div className='w-full h-screen'>
             <div className='flex text-2xl font-bold text-white gap-5 p-5 justify-center' >
                 <div className='bg-indigo-500 p-4 rounded'>Login Page</div>
+            </div>
+            <form className="flex justify-center gap-2 px-4">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                            Last Name
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+                    </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full px-3">
+                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                            Password
+                        </label>
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                        <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                    </div>
+                </div>
+            </form>
+            <div className='flex text-2xl font-bold text-white gap-5 p-5 justify-center' >
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Sign in
+                </button>
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Log out
+                </button>
             </div>
         </div>
     )
