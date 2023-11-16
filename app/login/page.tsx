083c61695017a91f7c5baed560620b05b90a5229
Humberto Asca <hasca@ciplima.org.pe>
2023-11-16 16:51:20 -0500

    Adding layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                             User Name
                         </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="What's your username?" />
                     </div>
                 </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
